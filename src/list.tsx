9e9e364d0c46f319f04000339d6f43e64b773f86
Simon Kubiak <kubiak@sipgate.de>
2024-02-05 22:38:32 +0000

    activate version

--- src/list.tsx
+++ src/list.tsx
@@ -1,21 +1,52 @@
-import { List } from "@raycast/api";
+import { Action, ActionPanel, Icon, List, showToast, Toast } from "@raycast/api";
 import { nClient } from "./util/nClient";
 import { useEffect, useState } from "react";
+import Style = Toast.Style;
 
 export default function Command() {
   const [versions, setVersions] = useState<string[] | undefined>();
+  const [activeVersion, setActiveVersion] = useState<string | undefined>();
 
   useEffect(() => {
-    (async () => {
-      const versions = nClient.listLocalVersions();
-      setVersions(versions);
-    })();
+    setVersions(nClient.getLocalVersions());
+    setActiveVersion(nClient.getActiveVersion());
   }, []);
+
+  async function activateVersion(version: string) {
+    const toast = await showToast(Style.Animated, `Activating version ${version}`, "Hang on…");
+    const success = nClient.setActiveVersion(version);
+
+    if (success) {
+      toast.title = `Activated Version ${version}`;
+      toast.message = undefined;
+      toast.style = Style.Success;
+      setActiveVersion(version);
+    } else {
+      toast.title = `Activating Version ${version} failed`;
+      toast.message = undefined;
+      toast.style = Style.Failure;
+    }
+  }
+
+  function getVersionIcon(version: string) {
+    return version == activeVersion ? Icon.CheckCircle : Icon.Circle;
+  }
 
   return (
     <List isLoading={versions == undefined}>
       {versions?.map((version) => {
-        return <List.Item key={version} title={version} />;
+        return (
+          <List.Item
+            key={version}
+            title={version}
+            icon={getVersionIcon(version)}
+            actions={
+              <ActionPanel>
+                <Action title="Activate Version" icon={Icon.CheckCircle} onAction={() => activateVersion(version)} />
+              </ActionPanel>
+            }
+          />
+        );
       })}
     </List>
   );
--- src/util/nClient.ts
+++ src/util/nClient.ts
@@ -5,24 +5,49 @@ import * as os from "os";
 class NClient {
   public readonly isInstalled: boolean;
   private readonly nPath: string;
+  private readonly nDirectory: string;
 
-  constructor(nPath: string) {
-    console.log(`NClient constructor called with nPath ${nPath}.`);
+  constructor(nPath: string, nDirectory: string) {
+    console.log(`NClient constructor called with nPath ${nPath} and nDirectory ${nDirectory}.`);
 
+    this.nDirectory = nDirectory;
     this.nPath = nPath;
 
     this.isInstalled = this.isNInstalled();
   }
 
-  listLocalVersions() {
+  getLocalVersions() {
     const { stdout } = this.runCommand("ls");
+    const localVersions = stdout.toString().trim().replaceAll("node/", "").split(os.EOL);
 
-    return stdout.toString().replaceAll("node/", "").split(os.EOL);
+    console.log(`getLocalVersions: ${localVersions}`);
+
+    return localVersions;
+  }
+
+  setActiveVersion(version: string): boolean {
+    const { status } = this.runCommand(version);
+
+    return status === 0;
+  }
+
+  getActiveVersion(): string | undefined {
+    const nodeExecutable = `${this.nDirectory}/bin/node`;
+    const { stdout, status } = spawnSync(`${nodeExecutable} --version`, { shell: true });
+
+    if (status === 0) {
+      // vXX.YY.ZZ\n
+      const activeVersion = stdout.toString().replace("v", "").trim();
+      console.log(`getActiveVersion: ${activeVersion}`);
+      return activeVersion;
+    } else {
+      console.error("Could not get active version");
+    }
   }
 
   private runCommand(command: string) {
-    const { stdout, stderr, error } = spawnSync(`${this.nPath} ${command}`, { shell: true });
-    return { stdout, stderr, error };
+    const { stdout, stderr, error, status } = spawnSync(`${this.nPath} ${command}`, { shell: true });
+    return { stdout, stderr, error, status };
   }
 
   private isNInstalled() {
@@ -38,6 +63,6 @@ class NClient {
   }
 }
 
-const nClient = new NClient(preferences.path);
+const nClient = new NClient(preferences.path, preferences.directory);
 
 export { nClient };
